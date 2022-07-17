8858c61fbc4bde6a42f6386759f56c8324b96470
FadhilBotz <92239117+FadhilBotz@users.noreply.github.com>
2022-07-17 07:34:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -37,11 +37,11 @@ global.eror = '_*𝒎𝒂𝒂𝒇 𝒔𝒆𝒓𝒗𝒆𝒓 𝒆𝒓𝒓𝒐𝒓.
 
 //========Url Template Buttons=========//
 global.dtu = '𝑌𝑜𝑢𝑡𝑢𝑏𝑒'
-global.urlnya = "https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ"
+global.urlnya = "https://youtube.com/channel/UCEuYh2NItW42-f5OMvbCo6Q"
 
 //============= callButtons =============//
 global.dtc = '𝒄𝒂𝒍𝒍 𝒐𝒘𝒏𝒆𝒓'
-global.phn = '62 857-1313-7903'
+global.phn = '62 887-0823-1116'
 
 //============= Games ================//
 global.benar = '_*Benar*_'
